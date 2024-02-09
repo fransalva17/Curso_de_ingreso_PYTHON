81604b9881e41cec80769fb82687b428202c5c5b
User <fransaalvatierra@gmail.com>
2024-02-09 20:20:41 -0300

    if 01

--- 00-Unidades/02_if/Ejercicios_If/IF_app_01.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_01.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: franco
+apellido: salvatierra
 ---
 Ejercicio: if_01
 ---
@@ -33,8 +33,13 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        txt_edad = self.txt_edad.get()
+        edad = int(txt_edad)
 
+        if edad == 18:
+            alert("UTN","Usted tiene 18 años")
+        else:
+            alert("UTN","usted no tiene 18 años")
          
 if __name__ == "__main__":
     app = App()
